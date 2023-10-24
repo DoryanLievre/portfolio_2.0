7a910c5dafcd20c8301e431fdf35c3b2964e753e
Doryan L <dlievre.dev@gmail.com>
2023-10-24 10:37:47 +0200

    feat(page.tsx): add background color

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Image from 'next/image'
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-[#242424] text-white h-screen">
       <Header />
+        <section id="hero">
+          <Hero />
+        </section>
     </main>
   )
 }
