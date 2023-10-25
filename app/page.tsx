2b4f2983278adba21afc699fe0c9c9a56f26a26a
Doryan L <dlievre.dev@gmail.com>
2023-10-25 18:27:18 +0200

    feat(Header): add scroll to top link and footer

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,11 @@ import StudyExperience from "@/components/StudyExperience";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
 import ContactMe from "@/components/ContactMe";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="bg-[#242424] text-white h-screen snap-y snap-mandatory overflow-x-hidden overflow-scroll z-o scrollbar scrollbar-track-gray-400/20 scrollbar-corner-gray-400/20 scrollbar-thumb-[#22d3ee]/80 scrollbar-thin  ">
+    <main className="bg-[#242424] text-white h-screen snap-y snap-mandatory overflow-x-hidden overflow-scroll z-o scrollbar scrollbar-track-gray-400/20 scrollbar-corner-gray-400/20 scrollbar-thumb-[#22d3ee]/80 scrollbar-thin scroll-smooth  ">
       <Header />
 
       <section id="hero" className="snap-start">
@@ -38,6 +39,15 @@ export default function Home() {
       <section id="contact" className="snap-start">
         <ContactMe />
       </section>
+
+        <Link href="#hero">
+            <div className="fixed bottom-5 right-5 cursor-pointer hidden md:block z-50">
+                <img className="h-12 w-12 xl:w-16 xl:h-16 bg-[#22d3ee]/50 rounded-full filter grayscale hover:grayscale-0 cursor-pointer" src="/images/noun-arrow-top.png" alt=""/>
+            </div>
+        </Link>
+        <footer className="sticky w-screen flex items-center justify-center bg-gray-500/20 snap-center ">
+            <p className="text-2xl text-[#22d3ee]/50 font-semibold tracking-[2px]  py-10">Merci d'avoir scroller jusqu'ici !</p>
+        </footer>
     </main>
   );
 }
