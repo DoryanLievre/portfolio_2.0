6616386fad6508171f4ae3c5baf4adbb5cbcc40e
Doryan L <dlievre.dev@gmail.com>
2023-10-26 13:24:42 +0200

    feat(sanity): build

---
+++ dist/static/index-27f558d4-400e2b8b.js
@@ -0,0 +1,1 @@
+import{DocumentPane as t}from"./index-f6f16e84-f35b7098.js";import{D as m}from"./sanity-08524483.js";import"./json-inspector-cdec79b4.js";export{m as DocumentPaneProvider,t as default};
--- dist/static/index-27f558d4-f0d175fa.js
+++
@@ -1,1 +0,0 @@
-import{DocumentPane as t}from"./index-f6f16e84-8d1f3adf.js";import{D as m}from"./sanity-a5e8ba39.js";import"./json-inspector-3db72223.js";export{m as DocumentPaneProvider,t as default};
