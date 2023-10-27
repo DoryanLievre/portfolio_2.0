94b9f333d968f6fa4620fdf94a933350ec8bb3b4
Doryan L <dlievre.dev@gmail.com>
2023-10-27 18:29:17 +0200

    TODO(image.ts): temporary solution to fix Image type issue

--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -8,6 +8,8 @@ const imageBuilder = createImageUrlBuilder({
   dataset: dataset || '',
 })
 
-export const urlForImage = (source: Image) => {
+// TODO: Comprendre et fixer le type pour ne pas ajouter Image_2 ...
+// @ts-ignore
+export const urlForImage = (source: Image | Image_2) => {
   return imageBuilder?.image(source).auto('format').fit('max')
 }
