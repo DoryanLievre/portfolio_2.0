4371696b305ad53a33e3d22bd52e304855545cdf
Doryan L <dlievre.dev@gmail.com>
2023-10-27 18:30:07 +0200

    feat(sanity env.ts): replace .env value

--- sanity/env.ts
+++ sanity/env.ts
@@ -3,15 +3,15 @@ export const apiVersion =
 
 export const dataset = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
+  'Missing environment variable: SANITY_STUDIO_DATASET'
 )
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  'Missing environment variable: SANITY_STUDIO_PROJECT_ID'
 )
 
-export const useCdn = true
+export const useCdn = false
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
