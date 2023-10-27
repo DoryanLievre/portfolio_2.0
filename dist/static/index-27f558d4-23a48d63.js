8384bb31b77db8d35943c3cfae6f64173e1d10c9
Doryan L <dlievre.dev@gmail.com>
2023-10-27 18:25:42 +0200

    sanity build

--- dist/static/index-27f558d4-23a48d63.js
+++
@@ -1,1 +0,0 @@
-import{DocumentPane as t}from"./index-f6f16e84-812d7e5f.js";import{D as m}from"./sanity-8b82fe53.js";import"./json-inspector-37cfb3c1.js";export{m as DocumentPaneProvider,t as default};
---
+++ dist/static/index-27f558d4-c0c3a412.js
@@ -0,0 +1,1 @@
+import{DocumentPane as t}from"./index-f6f16e84-ee788d5e.js";import{D as m}from"./sanity-252f08a6.js";import"./json-inspector-18750487.js";export{m as DocumentPaneProvider,t as default};
